da3231256c96b79420042fcaaa2c2d0e09d905b8
chukmunnlee <chukmunnlee@gmail.com>
2022-11-05 14:38:17 +0800

    AnimationPlayer

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,18 +6,39 @@ const ACCELERATION = 500
 const FRICTION = 500
 
 var velocity = Vector2.ZERO
+var animationPlayer = null 
+var animationName = "IdleRight"
+# alternative to _ready() function
+# onready var animationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("player is ready")
+	# $ to get a node in the same scene
+	# only accessible when the game starts
+	# or use the onready var
+	animationPlayer = $AnimationPlayer 
 
 # called every tick 1/60 sec
 func _physics_process(delta):
+	
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") 
 	# normalized the vector the unit lenght, reduced the diagonal speed of the sprite
 	input_vector = input_vector.normalized()
+	
+	if input_vector.x > 0:
+		animationName = "RunRight"
+	elif input_vector.x < 0:
+		animationName = "RunLeft"
+	else:
+		if animationName == "RunRight":
+			animationName = "IdleRight"
+		elif animationName == "RunLeft":
+			animationName = "IdleLeft"
+			
+	animationPlayer.play(animationName)
 	
 	if input_vector == Vector2.ZERO:
 		# friction, if not pressing any keys, reduce the velocity slowly
