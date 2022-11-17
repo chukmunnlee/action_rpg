90084cb8db372ce8ee1fe321a2c6b34a90bacce8
chukmunnlee <chukmunnlee@gmail.com>
2022-11-17 08:48:59 +0800

    Attacking Animation + State Machines

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,7 +5,15 @@ const MAX_SPEED = 80
 const ACCELERATION = 500
 const FRICTION = 500
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+var state = MOVE
+
 var velocity = Vector2.ZERO
+
 var animationPlayer = null 
 var animationTree = null
 var animationState = null
@@ -22,10 +30,22 @@ func _ready():
 	animationTree = $AnimationTree
 	# get the AnimationPlayer from the AnimationTree, this is the FSM
 	animationState = animationTree.get("parameters/playback")
+	# start the animation tree when the game starts
+	animationTree.active = true
 
 # called every tick 1/60 sec
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+			
+		ATTACK:
+			attack_state()
+			
+		ROLL:
+			pass
 	
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") 
@@ -42,6 +62,7 @@ func _physics_process(delta):
 		# add .1 to the Y axis in the animation tree (Idle/Run) to get it to prioritize left/right over up/down
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		# Run is the node name in the AnimationTree
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
@@ -53,3 +74,15 @@ func _physics_process(delta):
 	# move_and_slide() will handle the delta inside the function
 	# returned the velocity after the collision
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+func attack_state():
+	# stand still when attacking
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+	
+func attack_animation_finished():
+	# don't forget to disable looping in the animation
+	state = MOVE
